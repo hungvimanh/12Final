e54e5c9eaedc925e28db741b4e3378f104aff433
hungvm <hungvimanh.cntt@gmail.com>
2019-11-14 22:10:04 +0700

    add CORS

--- TwelveFinal/TwelveFinal/Startup.cs
+++ TwelveFinal/TwelveFinal/Startup.cs
@@ -126,6 +126,7 @@ namespace TwelveFinal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors(AllowSpecificOrigins);
             app.UseHttpsRedirection();
             app.UseMiddleware<ErrorHandlingMiddleware>();
             app.UseAuthentication();
